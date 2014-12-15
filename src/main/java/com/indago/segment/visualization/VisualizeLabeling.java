3b6011149c60ed4d942613788732cef55d225205
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2014-12-15 13:55:47 +0100

    loosen type requirements for VisualizeLabeling

--- src/main/java/com/indago/segment/visualization/VisualizeLabeling.java
+++ src/main/java/com/indago/segment/visualization/VisualizeLabeling.java
@@ -12,40 +12,32 @@ import net.imglib2.newlabeling.LabelingType;
 import net.imglib2.newlabeling.NativeImgLabeling;
 import net.imglib2.type.Type;
 
-public class VisualizeLabeling
-{
+public class VisualizeLabeling {
 
-	public static < C extends Type< C >, L extends Comparable< L > > void colorLabels( final NativeImgLabeling< L, ? > labeling, final Iterator< C > colors, final RandomAccessibleInterval< C > regionsImg )
-	{
+	public static < C extends Type< C >, L > void colorLabels( final NativeImgLabeling< L, ? > labeling, final Iterator< C > colors, final RandomAccessibleInterval< C > regionsImg ) {
 		final HashMap< Set< ? >, C > colorTable = new HashMap< Set< ? >, C >();
 		final LabelingMapping< ? > mapping = labeling.getMapping();
 		final int numLists = mapping.numSets();
-		for ( int i = 0; i < numLists; ++i )
-		{
+		for ( int i = 0; i < numLists; ++i ) {
 			final Set< ? > list = mapping.labelsAtIndex( i );
 			colorTable.put( list, colors.next() );
 		}
 
-		new RandomAccessibleProjector2D< LabelingType< L >, C >( 0, 1, labeling, regionsImg,
-				new LabelingTypeConverter< L, C >( colorTable )
-				).map();
+		new RandomAccessibleProjector2D< LabelingType< L >, C >( 0, 1, labeling, regionsImg, new LabelingTypeConverter< C >( colorTable ) ).map();
 	}
 
-	public static class LabelingTypeConverter< L extends Comparable< L >, T extends Type< T > > implements Converter< LabelingType< L >, T >
-	{
+	public static class LabelingTypeConverter< T extends Type< T > > implements Converter< LabelingType< ? >, T > {
+
 		private final HashMap< Set< ? >, T > colorTable;
 
-		public LabelingTypeConverter( final HashMap< Set< ? >, T > colorTable )
-		{
+		public LabelingTypeConverter( final HashMap< Set< ? >, T > colorTable ) {
 			this.colorTable = colorTable;
 		}
 
 		@Override
-		public void convert( final LabelingType< L > input, final T output )
-		{
+		public void convert( final LabelingType< ? > input, final T output ) {
 			final T t = colorTable.get( input );
-			if ( t != null )
-				output.set( t );
+			if ( t != null ) output.set( t );
 		}
 	}
 }
