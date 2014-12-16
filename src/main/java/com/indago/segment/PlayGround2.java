3f1fea1705332737250df28f6d10afb015cc13b0
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2014-12-16 19:14:28 +0100

    build and solve ILP and create solution Assignment

--- src/main/java/com/indago/segment/PlayGround2.java
+++ src/main/java/com/indago/segment/PlayGround2.java
@@ -1,5 +1,12 @@
 package com.indago.segment;
 
+import gurobi.GRB;
+import gurobi.GRB.DoubleAttr;
+import gurobi.GRBEnv;
+import gurobi.GRBException;
+import gurobi.GRBLinExpr;
+import gurobi.GRBModel;
+import gurobi.GRBVar;
 import io.scif.img.ImgOpener;
 
 import java.awt.BorderLayout;
@@ -20,8 +27,17 @@ import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.UnsignedIntType;
 
+import com.indago.fg.Assignment;
 import com.indago.fg.FactorGraph;
+import com.indago.fg.factor.BooleanFactor;
+import com.indago.fg.factor.Factor;
+import com.indago.fg.function.BooleanConflictConstraint;
+import com.indago.fg.function.BooleanFunction;
+import com.indago.fg.function.BooleanTensorTable;
 import com.indago.fg.gui.FgPanel;
+import com.indago.fg.value.BooleanValue;
+import com.indago.fg.variable.BooleanVariable;
+import com.indago.fg.variable.Variable;
 import com.indago.segment.fg.FactorGraphFactory;
 import com.indago.segment.filteredcomponents.FilteredComponentTree;
 
@@ -72,6 +88,8 @@ public class PlayGround2 {
 		final RandomSegmentCosts costs = new RandomSegmentCosts( segmentMultiForest, 815 );
 		final FactorGraph fg = FactorGraphFactory.createFromSegmentMultiForest( segmentMultiForest, costs, false );
 
+		buildILP( fg );
+
 		final JFrame guiFrame = new JFrame( "FG from SegmentMultiForest" );
 		// Set window-closing action...
 		guiFrame.addWindowListener( new WindowAdapter() {
@@ -86,6 +104,90 @@ public class PlayGround2 {
 		guiFrame.getContentPane().add( new FgPanel( fg ), BorderLayout.CENTER );
 		guiFrame.setSize( 800, 600 );
 		guiFrame.setVisible( true );
+	}
+
+	private static void buildILP( final FactorGraph fg ) throws GRBException {
+		for ( final Variable< ? > variable : fg.getVariables() ) {
+			if ( !( variable instanceof BooleanVariable ) )
+				throw new IllegalArgumentException();
+		}
+		final ArrayList< BooleanFactor > constraints = new ArrayList< BooleanFactor >();
+		final ArrayList< BooleanFactor > unaries = new ArrayList< BooleanFactor >();
+		for ( final Factor< ?, ?, ? > f : fg.getFactors() ) {
+			if ( f instanceof BooleanFactor ) {
+				final BooleanFactor factor = ( BooleanFactor ) f;
+				final BooleanFunction function = factor.getFunction();
+				if ( function instanceof BooleanConflictConstraint )
+					constraints.add( factor );
+				else if ( function instanceof BooleanTensorTable )
+					unaries.add( factor );
+				else
+					throw new IllegalArgumentException();
+			} else
+				throw new IllegalArgumentException();
+		}
+
+		final List< BooleanVariable > variables = ( List< BooleanVariable > ) fg.getVariables();
+		final HashMap< BooleanVariable, Integer > variableToIndex = new HashMap<>();
+		int variableIndex = 0;
+		for ( final BooleanVariable v : variables )
+			variableToIndex.put( v, variableIndex++ );
+
+		final GRBEnv env = new GRBEnv( "mip1.log" );
+		final GRBModel model = new GRBModel( env );
+
+		// Create variables
+		final GRBVar[] vars = model.addVars( variables.size(), GRB.BINARY );
+
+		// Integrate new variables
+		model.update();
+
+		// Set objective: minimize costs
+		final double[] coeffs = new double[ variables.size() ];
+		for ( final BooleanFactor factor : unaries )
+		{
+			final int i = variableToIndex.get( factor.getVariable( 0 ) );
+			final BooleanTensorTable costs = ( BooleanTensorTable ) factor.getFunction();
+			coeffs[ i ] = costs.evaluate( BooleanValue.TRUE ) - costs.evaluate( BooleanValue.FALSE );
+		}
+		final GRBLinExpr expr = new GRBLinExpr();
+		expr.addTerms( coeffs, vars );
+		model.setObjective( expr, GRB.MINIMIZE );
+
+		// Add constraints.
+		for ( int i = 0; i < constraints.size(); i++ ) {
+			final BooleanFactor constraint = constraints.get( i );
+			final GRBLinExpr lhsExprs = new GRBLinExpr();
+			for ( final BooleanVariable variable : constraint.getVariables() )
+			{
+				final int vi = variableToIndex.get( variable );
+				lhsExprs.addTerm( 1.0, vars[ vi ] );
+			}
+			model.addConstr( lhsExprs, GRB.LESS_EQUAL, 1.0, null );
+		}
+
+		// Optimize model
+		model.optimize();
+		System.out.println( "Obj: " + model.get( GRB.DoubleAttr.ObjVal ) );
+
+		// Build assignment
+		final Assignment assignment = new Assignment( variables );
+		for ( int i = 0; i < variables.size(); i++ ) {
+			final BooleanVariable variable = variables.get( i );
+			final BooleanValue value = vars[ i ].get( DoubleAttr.X ) > 0.5 ?
+					BooleanValue.TRUE :
+					BooleanValue.FALSE;
+			assignment.assign( variable, value );
+
+			System.out.println( variable + " = " + assignment.getAssignment( variable ) );
+		}
+
+		// Dispose of model and environment
+		model.dispose();
+		env.dispose();
+
+
+
 	}
 
 	static class ShowConflicts {
