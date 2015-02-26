8f38a8d32fe46766bdb82e281e52019dd554210d
Florian Jug <jug@mpi-cbg.de>
2015-02-26 13:49:56 +0100

    Random forest does learn something more meaningful now.

--- src/test/java/com/indago/wekatest/SimpleDecisionForestExample.java
+++ src/test/java/com/indago/wekatest/SimpleDecisionForestExample.java
@@ -4,13 +4,11 @@
 package com.indago.wekatest;
 
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.PrintStream;
 
 import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
-import weka.filters.Filter;
-import weka.filters.unsupervised.attribute.Normalize;
-import weka.filters.unsupervised.attribute.ReplaceMissingValues;
 
 
 /**
@@ -23,25 +21,29 @@ public class SimpleDecisionForestExample {
 
 	public static void main( final String[] args ) throws Exception {
 		// load training data
-		final weka.core.Instances training_data = new weka.core.Instances(
-						new java.io.FileReader( "src/main/resources/synthetic/0001_z63/FeatureExampleOnRealSegments_traindata.arff" ) );
+		final Instances training_data =
+				new Instances(
+						new FileReader( "src/main/resources/synthetic/0001_z63/FeatureExampleOnRealSegments_traindata.arff" ) );
 
 		//load test data
-		final weka.core.Instances test_data = new weka.core.Instances(
-						new java.io.FileReader( "src/main/resources/synthetic/0001_z63/FeatureExampleOnRealSegments_testdata.arff" ) );
+		final Instances test_data =
+				new Instances(
+						new FileReader( "src/main/resources/synthetic/0001_z63/FeatureExampleOnRealSegments_testdata.arff" ) );
 
 		//Clean up training data
-		final ReplaceMissingValues replace = new ReplaceMissingValues();
-		replace.setInputFormat( training_data );
-		final Instances training_data_filter1 = Filter.useFilter( training_data, replace );
+//		final ReplaceMissingValues replace = new ReplaceMissingValues();
+//		replace.setInputFormat( training_data );
+//		final Instances training_data_filter1 = Filter.useFilter( training_data, replace );
 
 		//Normalize training data
-		final Normalize norm = new Normalize();
-		norm.setInputFormat( training_data_filter1 );
-		final Instances processed_training_data = Filter.useFilter( training_data_filter1, norm );
+//		final Normalize norm = new Normalize();
+//		norm.setInputFormat( training_data_filter1 );
+//		final Instances processed_training_data = Filter.useFilter( training_data_filter1, norm );
 
 		//Set class attribute for pre-processed training data
-		processed_training_data.setClassIndex( processed_training_data.numAttributes() - 1 );
+//		processed_training_data.setClassIndex( processed_training_data.numAttributes() - 1 );
+
+		training_data.setClassIndex( 0 );
 
 		//output to file
 		Output = new FileOutputStream( "src/main/resources/synthetic/0001_z63/test.txt" );
@@ -49,25 +51,28 @@ public class SimpleDecisionForestExample {
 
 		//build classifier
 		final RandomForest tree = new RandomForest();
-		tree.buildClassifier( processed_training_data );
+		tree.buildClassifier( training_data );
 
 		//Clean up test data
-		replace.setInputFormat( test_data );
-		final Instances test_data_filter1 = Filter.useFilter( test_data, replace );
+//		replace.setInputFormat( test_data );
+//		final Instances test_data_filter1 = Filter.useFilter( test_data, replace );
 
 		//Normalize test data
-		norm.setInputFormat( training_data_filter1 );
-		final Instances processed_test_data = Filter.useFilter( test_data_filter1, norm );
+//		norm.setInputFormat( training_data_filter1 );
+//		final Instances processed_test_data = Filter.useFilter( test_data_filter1, norm );
 
 		//Set class attribute for pre-processed training data
-		processed_test_data.setClassIndex( processed_test_data.numAttributes() - 1 );
+//		processed_test_data.setClassIndex( processed_test_data.numAttributes() - 1 );
 
-		//int num_correct=0;
-		for ( int i = 0; i < processed_test_data.numInstances(); i++ ) {
-			final weka.core.Instance currentInst = processed_test_data.instance( i );
+		test_data.setClassIndex( 0 );
+
+		int num_ones = 0;
+		for ( int i = 0; i < test_data.numInstances(); i++ ) {
+			final weka.core.Instance currentInst = test_data.instance( i );
 			final int predictedClass = ( int ) tree.classifyInstance( currentInst );
-			System.out.println( predictedClass );
-			file.println( "O" + predictedClass );
+			if ( predictedClass == 1 ) num_ones++;
+			System.out.println( String.format( "%d\t%d", predictedClass, num_ones ) );
+			file.println( String.format( "%d\t%d", predictedClass, num_ones ) );
 		}
 	}
 }
