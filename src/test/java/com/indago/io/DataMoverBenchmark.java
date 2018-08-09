4e53d6cf11bd0d83feaa44c17d22097b22348e18
Matthias Arzt <arzt@mpi-cbg.de>
2018-08-09 18:14:06 +0200

    Add benchmark for DataMover

---
+++ src/test/java/com/indago/io/DataMoverBenchmark.java
@@ -0,0 +1,48 @@
+package com.indago.io;
+
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.type.numeric.real.FloatType;
+import org.junit.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+@State(Scope.Benchmark)
+public class DataMoverBenchmark {
+
+	private final long[] dimensions = {1000, 1000};
+
+	Img<FloatType> floats = ArrayImgs.floats(dimensions);
+	Img<DoubleType > doubles = ArrayImgs.doubles(dimensions);
+	Img<UnsignedShortType > shorts = ArrayImgs.unsignedShorts(dimensions);
+	Img<IntType > ints = ArrayImgs.ints(dimensions);
+
+	@Benchmark
+	public void testFloatToDouble() throws Exception {
+		DataMover.convertAndCopy(floats, doubles);
+		DataMover.convertAndCopy(shorts, ints);
+		DataMover.convertAndCopy(shorts, floats);
+	}
+
+	public static void main( final String... args ) throws RunnerException
+	{
+		final Options opt = new OptionsBuilder()
+				.include( DataMover.class.getSimpleName() )
+				.forks( 0 )
+				.warmupIterations( 20 )
+				.measurementIterations( 20 )
+				.warmupTime( TimeValue.milliseconds( 100 ) )
+				.measurementTime( TimeValue.milliseconds( 100 ) )
+				.build();
+		new Runner( opt ).run();
+	}
+}
