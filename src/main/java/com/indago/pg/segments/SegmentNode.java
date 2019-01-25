0aca14e45a54cbc79c998191c426f928b3abf534
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2019-01-25 12:58:33 +0100

    Remove unused field and methods

--- src/main/java/com/indago/pg/segments/SegmentNode.java
+++ src/main/java/com/indago/pg/segments/SegmentNode.java
@@ -2,12 +2,10 @@ package com.indago.pg.segments;
 
 import com.indago.data.segmentation.LabelingSegment;
 import com.indago.pg.IndicatorNode;
-import com.indago.pg.SegmentationProblem;
 import com.indago.pg.assignments.AssignmentNodes;
 
 public class SegmentNode extends IndicatorNode {
 
-	private SegmentationProblem sp;
 	private final LabelingSegment segment;
 	private final AssignmentNodes in;
 	private final AssignmentNodes out;
@@ -18,7 +16,6 @@ public class SegmentNode extends IndicatorNode {
 	public SegmentNode( final LabelingSegment segment, final double cost ) {
 		super( cost );
 		this.segment = segment;
-		this.sp = null;
 		in = new AssignmentNodes();
 		out = new AssignmentNodes();
 	}
@@ -31,16 +28,7 @@ public class SegmentNode extends IndicatorNode {
 		return out;
 	}
 
-	public void setSegmentationProblem( final SegmentationProblem sp ) {
-		this.sp = sp;
-	}
-
-	public SegmentationProblem getSegmentationProblem() {
-		return sp;
-	}
-
 	public LabelingSegment getSegment() {
 		return segment;
 	}
-
 }
