f07476b4ccd944df066e77a1def1717cd27e12d8
tpietzsch <tobias.pietzsch@gmail.com>
2020-06-25 13:31:03 +0200

    XmlIoLabelingPlus: write LabelingMapping to separate file in binary format

--- src/main/java/com/indago/data/segmentation/XmlIoLabelingPlus.java
+++ src/main/java/com/indago/data/segmentation/XmlIoLabelingPlus.java
@@ -1,11 +1,17 @@
 package com.indago.data.segmentation;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import org.jdom2.Document;
@@ -44,6 +50,8 @@ public class XmlIoLabelingPlus {
 	static int totalNumLabelSets = 0;
 
 	public static final String SEGMENTLABELING_TAG = "SegmentLabeling";
+	public static final String SEGMENTLABELING_VERSION_ATTRIBUTE_NAME = "version";
+	public static final String SEGMENTLABELING_VERSION_ATTRIBUTE_CURRENT = "0.2";
 	public static final String BASEPATH_TAG = "BasePath";
 
 	public static final String INDEXIMG_TAG = "IndexImgFile";
@@ -51,6 +59,8 @@ public class XmlIoLabelingPlus {
 	public static final String LABELS_TAG = "Labels";
 
 	public static final String MAPPING_TAG = "Mapping";
+	public static final String MAPPING_BINARY_ATTRIBUTE_NAME = "binary";
+	public static final String LABELMAPFILE_TAG = "MappingFile";
 	public static final String MAPPING_ENTRY_TAG = "MappingEntry";
 	public static final String MAPPING_ENTRY_INDEX_TAG = "Index";
 	public static final String MAPPING_ENTRY_LABELS_TAG = "Labels";
@@ -92,7 +102,7 @@ public class XmlIoLabelingPlus {
 		final LabelingPlus labelingPlus = new LabelingPlus( indexImg );
 		final long t2 = System.currentTimeMillis();
 		System.out.println( "    open IndexImg  : " + (t2-t1) + " ms" );
-		fromXml( root, labelingPlus );
+		fromXml( root, labelingPlus, basePath );
 		final long t3 = System.currentTimeMillis();
 		System.out.println( "    fromXml        : " + (t3-t2) + " ms" );
 		return labelingPlus;
@@ -104,36 +114,53 @@ public class XmlIoLabelingPlus {
 		final String indexImgFilename =
 				xmlFilename.substring( 0, xmlFilename.length() - ".xml".length() ) + ".tif";
 		final File indexImgFile = new File( indexImgFilename );
-		final Document doc = new Document( toXml( labelingData, xmlFileDirectory, indexImgFile ) );
+		final String labelingMappingFilename =
+				xmlFilename.substring( 0, xmlFilename.length() - ".xml".length() ) + ".labelmap";
+		final File labelingMappingFile = new File( labelingMappingFilename );
+		final Document doc = new Document( toXml( labelingData, xmlFileDirectory, indexImgFile, labelingMappingFile ) );
 		final XMLOutputter xout = new XMLOutputter( Format.getPrettyFormat() );
 		xout.output( doc, new FileOutputStream( xmlFilename ) );
+
+		writeLabelingMapping( labelingData.getLabeling().getMapping(), labelingMappingFile );
 
 		final Img< IntType > img = ImgView.wrap( labelingData.getLabeling().getIndexImg(), null );
 		ImageSaver.saveAsTiff( indexImgFilename, img );
 	}
 
-	private void fromXml( final Element segmentLabeling, final LabelingPlus labelingPlus )
+	private void fromXml( final Element segmentLabeling, final LabelingPlus labelingPlus, final File basePath )
 			throws IOException {
+		final String version = segmentLabeling.getAttributeValue( SEGMENTLABELING_VERSION_ATTRIBUTE_NAME, "0" );
+
 		final long t0 = System.currentTimeMillis();
 		final TIntObjectMap< LabelData > idToLabelMap =
 				getIdToLabelMap( segmentLabeling, LABELS_TAG );
-		final long t1 = System.currentTimeMillis();
 
 		final Element mapping = segmentLabeling.getChild( MAPPING_TAG );
 		if ( mapping == null )
 			throw new IOException( "no <" + MAPPING_TAG + "> element found." );
 
-		final ArrayList< Set< LabelData > > labelSets = new ArrayList<>();
-		for ( final Element entry : mapping.getChildren( MAPPING_ENTRY_TAG ) ) {
-			final int i = XmlHelpers.getInt( entry, MAPPING_ENTRY_INDEX_TAG );
-			final int[] ids = XmlHelpers.getIntArray( entry, MAPPING_ENTRY_LABELS_TAG );
-			final HashSet< LabelData > labelSet = new HashSet<>( ids.length );
-			for ( final int id : ids )
-				labelSet.add( idToLabelMap.get( id ) );
+		final ArrayList< Set< LabelData > > labelSets;
+		final boolean binary = Boolean.parseBoolean( mapping.getAttributeValue( "binary" ) );
+		if ( binary && Objects.equals( version, SEGMENTLABELING_VERSION_ATTRIBUTE_CURRENT ) )
+		{
+			final File labelingMappingFile = XmlHelpers.loadPath( mapping, LABELMAPFILE_TAG, basePath );
+			labelSets = readLabelingMapping( labelingMappingFile, idToLabelMap );
+		}
+		else
+		{
+			labelSets = new ArrayList<>();
+			for ( final Element entry : mapping.getChildren( MAPPING_ENTRY_TAG ) )
+			{
+				final int i = XmlHelpers.getInt( entry, MAPPING_ENTRY_INDEX_TAG );
+				final int[] ids = XmlHelpers.getIntArray( entry, MAPPING_ENTRY_LABELS_TAG );
+				final HashSet< LabelData > labelSet = new HashSet<>( ids.length );
+				for ( final int id : ids )
+					labelSet.add( idToLabelMap.get( id ) );
 
-			while ( labelSets.size() <= i )
-				labelSets.add( null );
-			labelSets.set( i, labelSet );
+				while ( labelSets.size() <= i )
+					labelSets.add( null );
+				labelSets.set( i, labelSet );
+			}
 		}
 		final long t2 = System.currentTimeMillis();
 		System.out.println( "        build labelSets  : " + (t2-t0) + " ms" );
@@ -176,9 +203,18 @@ public class XmlIoLabelingPlus {
 			final LabelingPlus labelingPlus,
 			final File xmlFileDirectory,
 			final File indexImgFile ) {
+		return toXml( labelingPlus, xmlFileDirectory, indexImgFile, null );
+	}
+
+	private Element toXml(
+			final LabelingPlus labelingPlus,
+			final File xmlFileDirectory,
+			final File indexImgFile,
+			final File labelingMappingFile ) {
 		final LabelingMapping< LabelData > mapping = labelingPlus.getLabeling().getMapping();
 
 		final Element segmentLabeling = new Element( SEGMENTLABELING_TAG );
+		segmentLabeling.setAttribute( SEGMENTLABELING_VERSION_ATTRIBUTE_NAME, SEGMENTLABELING_VERSION_ATTRIBUTE_CURRENT );
 		segmentLabeling.addContent( XmlHelpers.pathElement(
 				BASEPATH_TAG,
 				xmlFileDirectory,
@@ -190,12 +226,25 @@ public class XmlIoLabelingPlus {
 		segmentLabeling.addContent( labelIdsElement( LABELS_TAG, mapping.getLabels() ) );
 
 		final Element indexMap = new Element( MAPPING_TAG );
-		int i = 0;
-		for ( final Set< LabelData > labelSet : mapping.getLabelSets() ) {
-			final Element entry = new Element( MAPPING_ENTRY_TAG );
-			entry.addContent( XmlHelpers.intElement( MAPPING_ENTRY_INDEX_TAG, i++ ) );
-			entry.addContent( labelIdsElement( MAPPING_ENTRY_LABELS_TAG, labelSet ) );
-			indexMap.addContent( entry );
+		if ( labelingMappingFile != null )
+		{
+			indexMap.setAttribute( MAPPING_BINARY_ATTRIBUTE_NAME, "true" );
+			indexMap.addContent( XmlHelpers.pathElement(
+					LABELMAPFILE_TAG,
+					labelingMappingFile,
+					xmlFileDirectory ) );
+		}
+		else
+		{
+			indexMap.setAttribute( MAPPING_BINARY_ATTRIBUTE_NAME, "false" );
+			int i = 0;
+			for ( final Set< LabelData > labelSet : mapping.getLabelSets() )
+			{
+				final Element entry = new Element( MAPPING_ENTRY_TAG );
+				entry.addContent( XmlHelpers.intElement( MAPPING_ENTRY_INDEX_TAG, i++ ) );
+				entry.addContent( labelIdsElement( MAPPING_ENTRY_LABELS_TAG, labelSet ) );
+				indexMap.addContent( entry );
+			}
 		}
 		segmentLabeling.addContent( indexMap );
 
@@ -205,6 +254,59 @@ public class XmlIoLabelingPlus {
 				labelingPlus.getLabelingForests() ) );
 
 		return segmentLabeling;
+	}
+
+	/**
+	 * File format:
+	 * int: number of labelsets
+	 * (labelset*)
+	 * labelset :=
+	 *   int: index of labelset
+	 *   int: number of labels n
+	 *   int[n]: label ids
+	 */
+	private void writeLabelingMapping( final LabelingMapping< LabelData > mapping, final File labelingMappingFile ) throws IOException
+	{
+		try ( final DataOutputStream dos = new DataOutputStream(
+				new BufferedOutputStream(
+						new FileOutputStream( labelingMappingFile ),
+						1024 * 1024 ) ) )
+		{
+			final List< Set< LabelData > > labelSets = mapping.getLabelSets();
+			dos.writeInt( labelSets.size() );
+			int i = 0;
+			for ( final Set< LabelData > labelSet : labelSets )
+			{
+				dos.writeInt( i++ );
+				dos.writeInt( labelSet.size() );
+				for ( final LabelData label : labelSet )
+					dos.writeInt( label.getId() );
+			}
+		}
+	}
+
+	private ArrayList< Set< LabelData > > readLabelingMapping( final File labelingMappingFile, final TIntObjectMap< LabelData > idToLabelMap ) throws IOException
+	{
+		try ( final DataInputStream dis = new DataInputStream(
+				new BufferedInputStream(
+						new FileInputStream( labelingMappingFile ),
+						1024 * 1024 ) ) )
+		{
+			final int numLabelSets = dis.readInt();
+			final ArrayList< Set< LabelData > > labelSets = new ArrayList<>( numLabelSets );
+			for ( int i = 0; i < numLabelSets; ++i )
+			{
+				final int index = dis.readInt();
+				final int numLabels = dis.readInt();
+				final HashSet< LabelData > labelSet = new HashSet<>( numLabels );
+				for ( int j = 0; j < numLabels; j++ )
+					labelSet.add( idToLabelMap.get( dis.readInt() ) );
+				while ( labelSets.size() <= index )
+					labelSets.add( null );
+				labelSets.set( i, labelSet );
+			}
+			return labelSets;
+		}
 	}
 
 	private File loadBasePath( final Element root, final File xmlFile ) {
